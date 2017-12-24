9ced4441f1a25f3e8ffe7469369e08663205f752
Buskcoin <keith.groves@jointheleague.org>
2017-12-24 13:46:02 -0800

    Add files via upload

---
+++ clear_everything_client.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+bash <(curl -s https://raw.githubusercontent.com/jointheleague/clear-everything/master/clear_everything.sh)
+
+#https://stackoverflow.com/questions/6442364/running-script-upon-login-mac
+#launchctl load ~/Library/LaunchAgents/com.user.loginscript.plist
